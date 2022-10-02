483dbed3bcae99602f90aaddcb99d814cf2a7ff7
Sagnik Das <sagnikdas@Sagniks-MacBook-Pro.local>
2022-10-03 00:41:01 +0530

    changes

--- src/main/java/com/example/KafkaSpringBoot/KafkaSpringBootApplication.java
+++ src/main/java/com/example/KafkaSpringBoot/KafkaSpringBootApplication.java
@@ -3,6 +3,7 @@ package com.example.KafkaSpringBoot;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.core.KafkaTemplate;
 
@@ -11,23 +12,6 @@ public class KafkaSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaSpringBootApplication.class, args);
-	}
-
-	/**
-	 * Sends message to Kafka
-	 */
-	@Bean
-	public CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate){
-		try {
-			System.out.printf("Inside KafkaSpringBootApplication:commandLineRunner()");
-			return args -> {
-				kafkaTemplate.send("airPlaneTopic", "Hello World");
-			};
-		}
-		catch (Exception e){
-			e.printStackTrace();
-			return null;
-		}
 	}
 
 }
---
+++ src/main/java/com/example/KafkaSpringBoot/standard/producer/KafkaProducer.java
@@ -0,0 +1,37 @@
+package com.example.KafkaSpringBoot.standard.producer;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConditionalOnProperty(value = "spring.producer.enabled", havingValue = "true", matchIfMissing = false)
+@RequiredArgsConstructor
+public class KafkaProducer {
+
+    @Value("${producer.topic.name}")
+    private String topicName;
+
+    @Value("${producer.message}")
+    private String message;
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    /**
+     * Sends message to Kafka
+     */
+    @EventListener(ApplicationStartedEvent.class)
+    public void produce() {
+        try {
+            System.out.println("Inside KafkaProducer:produce()");
+            kafkaTemplate.send(topicName, message);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
